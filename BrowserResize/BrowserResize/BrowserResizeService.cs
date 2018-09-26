28793d04bcbb0236770154bdd21ba1fa9fdd32df
Chris Sainty <chrissainty@users.noreply.github.com>
2018-09-26 22:51:10 +0100

    Initial commit

---
+++ BrowserResize/BrowserResize/BrowserResizeService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.JSInterop;
+using System;
+
+namespace BrowserResize
+{
+    public class BrowserResizeService
+    {
+        public static event Action OnResize;
+
+        public static int InnerHeight { get; set; }
+        public static int InnerWidth { get; set; }
+
+        [JSInvokable]
+        public static void OnBrowserResize()
+        {
+            Console.WriteLine("Browser has resized");
+            OnResize?.Invoke();           
+        }
+    }
+}
